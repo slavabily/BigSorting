b8f00fe77f6e499cd579c0c57338b270330ec7c6
slava bily <slavabily@me.com>
2021-12-13 21:53:35 +0200

    6 from 9 tests are passed

--- BigSorting/main.swift
+++ BigSorting/main.swift
@@ -9,20 +9,29 @@ import Foundation
 
 func bigSorting(unsorted: [String]) -> [String] {
     var intArr = [Int]()
+    var strAr = [String]()
     for i in unsorted {
-        intArr.append(Int(i)!)
+        if let int = Int(i) {
+            intArr.append(int)
+        } else {
+            strAr.append(i)
+        }
     }
     print(intArr)
+    print(strAr)
+    strAr.sort { $0.count < $1.count }
+    print(strAr)
+    
     intArr.sort()
     print(intArr)
     var strArr = [String]()
     for i in intArr {
         strArr.append(String(i))
     }
-    print(strArr)
+    let sumArr = strArr + strAr
     
-    return strArr
+    return sumArr
 }
 
-                  print(bigSorting(unsorted: ["1","200","150","3"]))
+    print(bigSorting(unsorted: ["31415926535897932384626433832795","26535897932384626433832795","1","3","10","3","5"]))
 
