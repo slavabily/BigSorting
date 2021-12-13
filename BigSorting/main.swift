12ec50ba561d70ef8dee01d957652ad3888075b3
slava bily <slavabily@me.com>
2021-12-13 14:55:31 +0200

    one test case is passed

--- BigSorting/main.swift
+++ BigSorting/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-print("Hello, World!")
+func bigSorting(unsorted: [String]) -> [String] {
+    var intArr = [Int]()
+    for i in unsorted {
+        intArr.append(Int(i)!)
+    }
+    print(intArr)
+    intArr.sort()
+    print(intArr)
+    var strArr = [String]()
+    for i in intArr {
+        strArr.append(String(i))
+    }
+    print(strArr)
+    
+    return strArr
+}
+
+                  print(bigSorting(unsorted: ["1","200","150","3"]))
 
